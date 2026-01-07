b7223d4144edeabe314b6df9e7a869a82997bad5
BASIL ALBADER <basilalbapro@gmail.com>
2026-01-07 20:04:00 +0100

    WIP

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,21 +1,11 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
 import { Observability } from "@mastra/observability";
-import { lucie } from "./agents/lucie-agents";
-// import {
-//   toolCallAppropriatenessScorer,
-//   completenessScorer,
-//   translationScorer,
-// } from "./scorers/lucie-scorer";
+import { lucie } from './agents/lucie-agents';
 
 export const mastra = new Mastra({
   workflows: {},
   agents: { lucie },
-  // scorers: {
-  //   toolCallAppropriatenessScorer,
-  //   completenessScorer,
-  //   translationScorer,
-  // },
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
