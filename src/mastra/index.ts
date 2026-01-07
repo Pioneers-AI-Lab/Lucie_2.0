57971b53d1b4ef08226d1cce377fc4f5ea177eb3
BASIL ALBADER <basilalbapro@gmail.com>
2026-01-07 19:54:15 +0100

    WIP

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,28 +1,27 @@
 import { Mastra } from "@mastra/core/mastra";
 import { PinoLogger } from "@mastra/loggers";
-import { LibSQLStore } from "@mastra/libsql";
-// import { Observability } from "@mastra/observability";
+import { Observability } from "@mastra/observability";
 import { lucie } from "./agents/lucie-agents";
-import {
-  toolCallAppropriatenessScorer,
-  completenessScorer,
-  translationScorer,
-} from "./scorers/weather-scorer";
+// import {
+//   toolCallAppropriatenessScorer,
+//   completenessScorer,
+//   translationScorer,
+// } from "./scorers/lucie-scorer";
 
 export const mastra = new Mastra({
   workflows: {},
   agents: { lucie },
-  scorers: {
-    toolCallAppropriatenessScorer,
-    completenessScorer,
-    translationScorer,
-  },
+  // scorers: {
+  //   toolCallAppropriatenessScorer,
+  //   completenessScorer,
+  //   translationScorer,
+  // },
   logger: new PinoLogger({
     name: 'Mastra',
     level: 'info',
   }),
-  // observability: new Observability({
-  //   // Enables DefaultExporter and CloudExporter for tracing
-  //   default: { enabled: true },
-  // }),
+  observability: new Observability({
+    // Enables DefaultExporter and CloudExporter for tracing
+    default: { enabled: true },
+  }),
 });
