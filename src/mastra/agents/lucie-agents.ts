e06ccfed09e96d1651720639f4cff643a0ff2e64
BASIL ALBADER <basilalbapro@gmail.com>
2026-01-09 09:05:11 +0100

    WIP

--- src/mastra/agents/lucie-agents.ts
+++ src/mastra/agents/lucie-agents.ts
@@ -1,8 +1,8 @@
 import { Agent } from "@mastra/core/agent";
 import { Memory } from '@mastra/memory';
 import { MCPClient } from '@mastra/mcp';
-import { getCohortDataTool } from '../tools/cohort-data-tool';
-import { getAiLabDataTool } from '../tools/ai-lab-data-tool';
+import { Composio } from '@composio/core';
+import { MastraProvider } from '@composio/mastra';
 import { lucieInstructions } from './lucie-instructions';
 
 const mcp = new MCPClient({
@@ -25,9 +25,29 @@ export const lucie = new Agent({
   // todo: change the anthropic env to use the one max gets
   // model: 'openai/gpt-4.1-mini',
   model: 'anthropic/claude-3-haiku-20240307',
-  tools: {
-    ...mcpTools,
-    getCohortDataTool,
-    getAiLabDataTool,
+  tools: async ({ requestContext }) => {
+    const composio = new Composio({
+      provider: new MastraProvider(),
+    });
+
+    // retrieve userId and activeAccount from the requestContext
+    const userId = requestContext.get<'userId', string>('userId');
+    const activeAccount = requestContext.get<
+      'activeAccount',
+      Awaited<
+        ReturnType<typeof composio.connectedAccounts.list>
+      >['items'][number]
+    >('activeAccount');
+
+    // return empty set of tools if activeAccount isn't present
+    if (!activeAccount) return {};
+
+    // fetch composio tools and dynamically use them in the agent
+    const composioTools = await composio.tools.get(userId, {
+      toolkits: [activeAccount.toolkit.slug],
+    });
+
+    return composioTools;
   },
+  ...mcpTools,
 });
