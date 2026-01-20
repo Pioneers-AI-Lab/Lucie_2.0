190f2d49018f20343b758eb7195df21b53b051c9
BASIL ALBADER <basilalbapro@gmail.com>
2026-01-20 15:06:40 +0100

    clean up

--- lib/airtable-api-client.ts
+++
@@ -1,9 +0,0 @@
-import ky from "ky";
-
-// ! NOTE: this was used for the Airtable API client, but we are now using airtable.js mcp client instead
-export const airtableApiClient = ky.create({
-  prefixUrl: 'https://api.airtable.com/v0/meta/bases/',
-  headers: {
-    Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
-  },
-});
